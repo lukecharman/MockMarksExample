2ce0401f526ef668e04a31a48b28713ab7d2018c
Luke Charman <Luke.Charman@mnscorp.netLuke.Charman@mnscorp.netLuke.Charman@mnscorp.net>
2023-02-23 15:41:18 +0000

    Strip the json files used for the tests in release builds so that they aren't included in the final binary.

--- StubbieSampler/StubbieSampler/SceneDelegate.swift
+++ StubbieSampler/StubbieSampler/SceneDelegate.swift
@@ -11,16 +11,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   ) {
     guard let windowScene = (scene as? UIWindowScene) else { return }
 
+    // 1. If we're UI testing, do Stubbie's global setup.
+    // 2. Set its session property to your app's URLSession instance.
     if Stubbie.isXCUITest {
       Stubbie.setUp()
       Stubbie.session = StubbieURLSession(stubbing: .shared)
     }
 
+    // 3. Use the Stubbie session if it's available in your app.
     let vm = ViewModel(urlSession: Stubbie.session ?? .shared)
     let vc = ViewController(viewModel: vm)
 
     let window = UIWindow(windowScene: windowScene)
-
     self.window = window
 
     window.rootViewController = vc
